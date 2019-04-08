59b6533a7005fb8846eab8a41382a535bcb8277e
shlarsson <simon.larsson@yh.nackademin.se>
2019-04-08 14:20:58 +0200

    Firebase sign in started

--- app/src/main/java/com/example/simon/attendence/LoginActivity.java
+++ app/src/main/java/com/example/simon/attendence/LoginActivity.java
@@ -5,13 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+       // updateUI(currentUser);
+    }
+
     void loginComplete(View view){
         Intent intent = new Intent(this, CheckinActivity.class);
         startActivity(intent);
